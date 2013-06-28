1829268bf1e3151e252af621bf83046124dac35f
goody808 <joshua.gudjohnsen@gmail.com>
2013-06-28 11:02:40 -0400

    changed name for folder

---
+++ test/models/restaurant_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+ 
+class RestaurantTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save restaurant without title" do
+  	restaurant = Restaurant.new
+  	assert !restaurant.save
+  end
+  
+end
