4b943eecf378ed95f493fbc541811e30797243e7
goody808 <joshua.gudjohnsen@gmail.com>
2014-04-10 15:52:39 -0400

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJCKU7XZH2XUNVN3A',                        # required
-    :aws_secret_access_key  => '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'                       # required
+    # required
     # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
